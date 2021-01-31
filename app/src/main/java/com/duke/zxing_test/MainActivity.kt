8b511825b4c5456e23eb2547350f0b4c11e42c6c
duke <136239173@qq.com>
2021-01-31 16:44:22 +0800

    init

--- app/src/main/java/com/duke/zxing_test/MainActivity.kt
+++ app/src/main/java/com/duke/zxing_test/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.duke.zxing_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.zxing.client.android.CaptureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, CaptureActivity::class.java))
+
     }
 }
