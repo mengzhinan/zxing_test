2b6e7392a717844c4fc88b03e57f8a0a2c8cc2e9
duke <136239173@qq.com>
2021-01-31 22:09:15 +0800

    delete result

--- zxingLib/src/main/java/com/google/zxing/client/android/PreferencesActivity.java
+++ zxingLib/src/main/java/com/google/zxing/client/android/PreferencesActivity.java
@@ -36,12 +36,7 @@ public final class PreferencesActivity extends Activity {
 
   public static final String KEY_PLAY_BEEP = "preferences_play_beep";
   public static final String KEY_VIBRATE = "preferences_vibrate";
-  public static final String KEY_COPY_TO_CLIPBOARD = "preferences_copy_to_clipboard";
   public static final String KEY_FRONT_LIGHT_MODE = "preferences_front_light_mode";
-  public static final String KEY_BULK_MODE = "preferences_bulk_mode";
-  public static final String KEY_REMEMBER_DUPLICATES = "preferences_remember_duplicates";
-  public static final String KEY_ENABLE_HISTORY = "preferences_history";
-  public static final String KEY_SUPPLEMENTAL = "preferences_supplemental";
   public static final String KEY_AUTO_FOCUS = "preferences_auto_focus";
   public static final String KEY_INVERT_SCAN = "preferences_invert_scan";  
   public static final String KEY_SEARCH_COUNTRY = "preferences_search_country";
@@ -51,7 +46,6 @@ public final class PreferencesActivity extends Activity {
   public static final String KEY_DISABLE_EXPOSURE = "preferences_disable_exposure";
   public static final String KEY_DISABLE_METERING = "preferences_disable_metering";
   public static final String KEY_DISABLE_BARCODE_SCENE_MODE = "preferences_disable_barcode_scene_mode";
-  public static final String KEY_AUTO_OPEN_WEB = "preferences_auto_open_web";
 
   @Override
   protected void onCreate(Bundle icicle) {
