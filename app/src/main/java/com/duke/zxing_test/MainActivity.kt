b3ddb963096d26644713091fb663ea9359b192ec
duke <136239173@qq.com>
2021-09-20 23:01:25 +0800

    init

--- app/src/main/java/com/duke/zxing_test/MainActivity.kt
+++ app/src/main/java/com/duke/zxing_test/MainActivity.kt
@@ -2,8 +2,9 @@ package com.duke.zxing_test
 
 import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.client.android.CaptureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
