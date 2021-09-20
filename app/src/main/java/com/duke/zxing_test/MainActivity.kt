8af44f5fe1b21d92d0a56a44184631f31d782d33
duke <136239173@qq.com>
2021-09-20 23:36:04 +0800

    init

--- app/src/main/java/com/duke/zxing_test/MainActivity.kt
+++ app/src/main/java/com/duke/zxing_test/MainActivity.kt
@@ -3,18 +3,31 @@ package com.duke.zxing_test
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.client.android.CaptureActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvScan: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvScan = findViewById(R.id.tv_scan)
+
+        tvScan?.setOnClickListener {
+            scan()
+        }
+
+        scan()
+    }
+
+    private fun scan() {
         DPermission.newInstance(this)
             .setCallback {
                 startActivity(Intent(this, CaptureActivity::class.java))
             }.startRequest(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
     }
 }
