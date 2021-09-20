557a0c169d59cbda4ae6f467473e022d4adaddb3
duke <136239173@qq.com>
2021-09-20 22:14:24 +0800

    init

--- app/src/main/java/com/duke/zxing_test/MainActivity.kt
+++ app/src/main/java/com/duke/zxing_test/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.duke.zxing_test
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DPermission.newInstance(this)
+            .setCallback {
+                startActivity(Intent(this, CaptureActivity::class.java))
+            }.startRequest(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
     }
 }
